48d406377567ce6263376d647cf173156e3aacb2
Anh Duy <noname.spyware@gmail.com>
2019-09-04 11:54:38 +0700

    [+] Init api_profile_update and api_profile_detail [+] init data for location include city, ward, district [+] Update .gitignore (remove .vscode folder) [+] Update urls include api_profile_update and api_profile_detail [+] Update location admin site [+] Update log format

--- apps/apis/urls.py
+++ apps/apis/urls.py
@@ -4,13 +4,14 @@ from django.urls import path
 from apps.apis.views.api_login import Login
 from apps.apis.views.api_logout import Logout
 from apps.apis.views.api_register import Register
+from apps.apis.views.api_profile_detail import ProfileDetail
 
 urlpatterns = [
-	path('register', Register.as_view()),
-	path('login', Login.as_view()),
-	path('logout', Logout.as_view()),
+	path('register/', Register.as_view()),
+	path('login/', Login.as_view()),
+	path('logout/', Logout.as_view()),
 	# path('profile/update/', ''),
-	# path('profile/detail/', ''),
+	path('profile/detail/', ProfileDetail.as_view()),
 	# path('friend/add/', ''),
 	# path('friend/remove/', ''),
 ]
